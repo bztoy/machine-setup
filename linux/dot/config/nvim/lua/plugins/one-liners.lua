c77d9aa15c14926112d8b9b1a35b89f64393c258
Wises Keshom <wises.keshom@gmail.com>
2025-06-19 18:34:37 +0700

    update nvim configs (more...)

--- linux/dot/config/nvim/lua/plugins/one-liners.lua
+++ linux/dot/config/nvim/lua/plugins/one-liners.lua
@@ -11,6 +11,9 @@ return {
     },
     { -- Git plugin
         'tpope/vim-fugitive',
+	config = function()
+	    vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+	end
     },
     { -- Show CSS Colors
         'brenoprata10/nvim-highlight-colors',
--- linux/dot/config/nvim/lua/plugins/telescope.lua
+++ linux/dot/config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,29 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim', branch = "0.1.x",
+      dependencies = { 
+	"nvim-lua/plenary.nvim",
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	"nvim-tree/nvim-web-devicons"
+      },
       config = function()
+          local telescope = require("telescope")
+          local actions = require("telescope.actions")
+
+	  telescope.setup({
+	    defaults = {
+	      path_display = { "smart" },
+	      mappings = {
+       	        i = {
+   		  ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+   	          ["<C-j>"] = actions.move_selection_next, -- move to next result
+   		  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+   	        },
+   	      },
+   	    },
+   	  })
+
+	  telescope.load_extension("fzf")
+
 	  local builtin = require('telescope.builtin')
 	  vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 	  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
